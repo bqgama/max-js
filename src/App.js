57bb7c7587cdb38a862b788b5fbb6b4ffcac5fde
bqgama <bqgama@gmail.com>
2023-02-21 00:16:43 -0300

    feature: lifting the state up in the component

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
 
   // return React.createElement(
   //   'div',
@@ -36,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
