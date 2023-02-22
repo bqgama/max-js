11650aeeea0f1dcc8c92ad592a0ba4a1c5a5eebf
bqgama <bqgama@gmail.com>
2023-02-22 10:15:51 -0300

    handling with conditions in NewExpense component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditinghandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditinghandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditinghandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={stopEditinghandler}
+        />
+      )}
     </div>
   );
 };
